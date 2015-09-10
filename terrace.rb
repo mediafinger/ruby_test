95c40d97da2cafdfb0f0f25b014696018f8c60e4
Andreas Finger <webmaster@mediafinger.com>
2015-09-10 22:05:49 +0200

    Add Terrace class and tests

--- terrace.rb
+++ terrace.rb
@@ -1,7 +1,32 @@
 class Terrace
-  attr_accessor :name
+  attr_reader :length, :width
 
-  def initialize()
+  def initialize(length, width)
+    @length = length
+    @width = width
+    @open = true
+  end
 
+  def size
+    @length * @width
+  end
+
+  def status
+    status = ""
+    if @open
+      status = "open"
+    else
+      status = "closed"
+    end
+
+    "The terrace is #{status}"
+  end
+
+  def close
+    @open = false
+  end
+
+  def open
+    @open = true
   end
 end
--- test/terrace_test.rb
+++ test/terrace_test.rb
@@ -3,8 +3,27 @@ require_relative "../terrace"
 
 class TerraceTest < Minitest::Test
   def setup
+    @terrace = Terrace.new(8, 3)
   end
 
-  def test_to_s
+  def test_size
+    assert_equal 24, @terrace.size
+  end
+
+  def test_status_new
+    assert_equal "The terrace is open", @terrace.status
+  end
+
+  def test_status_closed
+    @terrace.close
+
+    assert_equal "The terrace is closed", @terrace.status
+  end
+
+  def test_status_open
+    @terrace.close
+    @terrace.open
+
+    assert_equal "The terrace is open", @terrace.status
   end
 end
